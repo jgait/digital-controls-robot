a9a76db8a8a269268da0665ca2fdeca11dbfc61e
bcaro57 <bencaro@udel.edu>
2023-11-03 17:16:00 -0400

    Had to comment out the battery-breaker

--- Cookie_Monster_Arduino/src/main.cpp
+++ Cookie_Monster_Arduino/src/main.cpp
@@ -8,8 +8,9 @@ void setup() {
 }
 
 void loop() {
-	directionControl();
-	delay(1000);
+	// This function breaks the battery, so it's commented out for now.
+	// directionControl();
+	// delay(1000);
 	speedControl();
 	delay(1000);
 }
