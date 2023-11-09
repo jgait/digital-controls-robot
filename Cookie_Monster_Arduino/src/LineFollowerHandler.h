5738b2ed81c87806e3fcd2801d4b23bdd6b582c3
bcaro57 <bencaro@udel.edu>
2023-11-09 10:44:08 -0500

    finished the line follower example, which is wrote out as functions in the LineFollowerHandler header

--- Cookie_Monster_Arduino/src/LineFollowerHandler.h
+++ Cookie_Monster_Arduino/src/LineFollowerHandler.h
@@ -7,8 +7,6 @@ I have only input the setup and nothing else.
 */
 
 
-
-
 #include <Arduino.h>
 #include <sensorbar.h>
 
@@ -42,6 +40,69 @@ void LineFollowerSetup(){
   {
 	  Serial.println("The line follower I2C communication FAILED! :(");
 	  while(1);
+  }
+}
+
+void BarReading(){
+  //Wait 50 ms
+  delay(25);
+
+
+  //Get the data from the bar and save it to the circular buffer positionHistory.
+  int temp = mySensorBar.getDensity();
+  if( (temp < 4)&&(temp > 0) )
+  {
+    positionHistory.pushElement( mySensorBar.getPosition());
+  }
+
+
+  //print me a meter!
+  {
+    int16_t i;
+	//Get an average of the last 'n' readings
+    int16_t avePos = positionHistory.averageLast( 10 );
+    Serial.print("Scale = 5/char :");
+    for( i = -130; i <= 130; i = i + 5 )
+    {
+      if( i < 0 )
+      {
+        //if avePos within 5 of i
+        if((avePos > (i - 3)) && (avePos <= (i + 2)))
+        {
+          Serial.print("*");
+        }
+        else
+        {
+          Serial.print("-");
+        }
+      }
+      else if( i == 0 )
+      {
+        //if avePos within 5 of i
+        if((avePos > (i - 3)) && (avePos <= (i + 2)))
+        {
+          Serial.print("*");
+        }
+        else
+        {
+          Serial.print("+");
+        }
+      }
+      else if( i > 0 )
+      {
+        //if avePos within 5 of i
+        if((avePos > (i - 3)) && (avePos <= (i + 2)))
+        {
+          Serial.print("*");
+        }
+        else
+        {
+          Serial.print("-");
+        }
+      }
+    }
+    Serial.print(" avePos = ");
+    Serial.println(avePos);
   }
 }
 
--- Cookie_Monster_Arduino/src/main.cpp
+++ Cookie_Monster_Arduino/src/main.cpp
@@ -5,12 +5,15 @@
 void setup() {
 	Serial.begin(115200);
 	motorSetup();
+	LineFollowerSetup();
 }
 
 void loop() {
 	// This function breaks the battery, so it's commented out for now.
 	// directionControl();
 	// delay(1000);
-	speedControl();
-	delay(1000);
+
+	// speedControl();
+	// delay(1000);
+	BarReading();
 }
